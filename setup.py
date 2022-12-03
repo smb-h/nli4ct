094d18f28f5a1f125a0fb1d56e903b136fb1d983
SMB H <smbhop@pm.me>
2022-12-03 11:24:45 +0330

    :tada: new(project): initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nli4ct',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multi-evidence Natural Language Inference for Clinical Trial Data',
+    author='SMBH',
+    license='MIT',
+)
